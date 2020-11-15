2dca03a7c1d4150199fbdbf8734956a902920d5b
Rayhan Chowdhury <rayhan.chowdhury@kcl.ac.uk>
2020-11-15 20:45:49 +0000

    Webdriver IO Installation and Allure Reporter Installation

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -7,9 +7,9 @@ class LoginPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get inputUsername () { return $('#username') }
-    get inputPassword () { return $('#password') }
-    get btnSubmit () { return $('button[type="submit"]') }
+    get inputUsername () { return $('#p_username') }
+    get inputPassword () { return $('#password-input') }
+    get btnSubmit () { return $('#login-button') }
 
     /**
      * a method to encapsule automation code to interact with the page
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,18 +1,32 @@
 const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+// const SecurePage = require('../pageobjects/secure.page');
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
         LoginPage.open();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        LoginPage.login('ITPROFILE1', 'Kingscollegelondon2020!');
+
+        
+        // expect(SecurePage.flashAlert).toBeExisting();
+        // expect(SecurePage.flashAlert).toHaveTextContaining(
+        //     'You logged into a secure area!');
+
+        const elem = $('#corebutton-1082-btnInnerEl')
+        elem.waitUntil(function () {
+            return this.getText() === 'ASK A QUESTION'
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'Cannot find button "ASK A QUESTION"'
+        
+          });
+
+
     });
 
 
   //  npx wdio wdio.conf.js
+  // allure generate allure-results/ --clean && allure open
 });
 
 
